d212d2c124845e7637d7e431e72e1b7711bfbd50
1357koushik <1357koushik@gmail.com>
2026-07-09 00:55:39 +0530

    removed test files

--- debug_fuzz.py
+++
@@ -1,9 +0,0 @@
-from rapidfuzz import fuzz, utils
-print("Machine Learning vs Machine learning")
-print("Ratio:", fuzz.ratio("Machine Learning", "Machine learning"))
-print("Token Sort:", fuzz.token_sort_ratio("Machine Learning", "Machine learning"))
-print("Token Sort with processor:", fuzz.token_sort_ratio("Machine Learning", "Machine learning", processor=utils.default_process))
-
-print("\nElon Musk vs Musk, Elon")
-print("Token Sort:", fuzz.token_sort_ratio("Elon Musk", "Musk, Elon"))
-print("Token Sort with processor:", fuzz.token_sort_ratio("Elon Musk", "Musk, Elon", processor=utils.default_process))
